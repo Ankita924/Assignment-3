b07dd18fa5cf038cc2ffb111a59f580ab1875950
Ankita924 <ankitadutta924@gmail.com>
2025-04-13 13:00:12 +0530

    Add files via upload

---
+++ Task.2.py
@@ -0,0 +1,8 @@
+from math import *
+n=int(input('Enter a number: '))
+a=sqrt(n)
+b=log(n,e)
+c=sin(n)
+print('Square root of',n, 'is: ',a)
+print('Logarithm of',n, 'is: ',b)
+print('Sine of',n, 'is: ',c)
